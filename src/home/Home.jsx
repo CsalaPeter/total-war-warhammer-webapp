c5d67fd731fb035e40c6f0c017fe1eca6a26affc
CsalaPeter <csalapeter95@gmail.com>
2022-09-05 16:01:45 +0200

    Wip

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,65 @@
 import "../sass/global.scss";
+import races from "../data/races.json";
 
 function Home() {
-  return <h1 className="test">Home Page</h1>;
+  console.log(races);
+  return (
+    <div className="mainContainer">
+      <HomePageHeader />
+      <RacesAndFactions />
+    </div>
+  );
 }
 
 export default Home;
+
+const HomePageHeader = () => {
+  return (
+    <header className="logos">
+      <img src="/img/wh-logo.png" alt="Warhammer 1 Logo" className="wh1-logo" />
+      <img
+        src="/img/wh2-logo.png"
+        alt="Warhammer 2 Logo"
+        className="wh2-logo"
+      />
+
+      <img
+        src="/img/wh3-logo.png"
+        alt="Warhammer 3 Logo"
+        className="wh3-logo"
+      />
+    </header>
+  );
+};
+
+const RacesAndFactions = () => {
+  return (
+    <div className="contentContainer">
+      <header>
+        <h2 className="contentHeader">Races And Factions</h2>
+      </header>
+      <div className="listing">
+        {races.map((race, key) => {
+          return (
+            <div className="cardHolder" key={key}>
+              <RaceCard
+                name={race.name}
+                factions={race.factions}
+                image={race.img}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const RaceCard = ({ name, factions }) => {
+  return (
+    <div className="wrapper">
+      <div className="cardName">{name}</div>
+      <div className="factions">{factions}</div>
+    </div>
+  );
+};
